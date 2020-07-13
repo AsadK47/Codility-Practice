bda4812e42ad13a453463b8cac1f7cc14facfc27
Asad Khan <asad_khan47@hotmail.co.uk>
2020-07-13 19:26:16 +0100

    TapeEquilibrium with O(N) complexity found on Google

--- src/timeComplexity/TapeEquilibrium.java
+++ src/timeComplexity/TapeEquilibrium.java
@@ -1,35 +1,58 @@
 package timeComplexity;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+/**
+ * Solution with O(N * N) complexity
+ *
+ * public class TapeEquilibrium {
+ *     public static int solution(int[] A) {
+ *         if (A.length == 0) {
+ *             return 1;
+ *         }
+ *
+ *         List<Integer> myArrayList = Arrays.stream(A).boxed().collect(Collectors.toList());
+ *         ArrayList<Integer> newList = new ArrayList<>();
+ *         List<Integer> sublist1;
+ *         List<Integer> sublist2;
+ *         int sum;
+ *         int sum2;
+ *         int absoluteSubtraction;
+ *
+ *         for (int i = 0; i < myArrayList.size() - 1; i++) {
+ *             sublist1 = myArrayList.subList(0, i + 1);
+ *             sublist2 = myArrayList.subList(i + 1, myArrayList.size());
+ *             sum = sublist1.stream().mapToInt(a -> a).sum();
+ *             sum2 = sublist2.stream().mapToInt(b -> b).sum();
+ *             absoluteSubtraction = Math.abs(sum - sum2);
+ *             newList.add(absoluteSubtraction);
+ *         }
+ *
+ *         Collections.sort(newList);
+ *         return newList.get(0);
+ *     }
+ * }
+ */
+
 
 public class TapeEquilibrium {
     public static int solution(int[] A) {
-        if (A.length == 0) {
-            return 1;
+        long sumOfAllElements = 0;
+
+        for (int value : A) {
+            sumOfAllElements += value;
         }
 
-        List<Integer> myArrayList = Arrays.stream(A).boxed().collect(Collectors.toList());
-        ArrayList<Integer> newList = new ArrayList<>();
-        List<Integer> sublist1;
-        List<Integer> sublist2;
-        int sum;
-        int sum2;
-        int absoluteSubtraction;
+        int minDifference = Integer.MAX_VALUE;
+        int currentDifference;
+        long sumOfFirstPart = 0;
+        long sumOfSecondPart;
 
-        for (int i = 0; i < myArrayList.size() - 1; i++) {
-            sublist1 = myArrayList.subList(0, i + 1);
-            sublist2 = myArrayList.subList(i + 1, myArrayList.size());
-            sum = sublist1.stream().mapToInt(a -> a).sum();
-            sum2 = sublist2.stream().mapToInt(b -> b).sum();
-            absoluteSubtraction = Math.abs(sum - sum2);
-            newList.add(absoluteSubtraction);
+        for (int p = 0; p < A.length - 1; p++) {
+            sumOfFirstPart += A[p];
+            sumOfSecondPart = sumOfAllElements - sumOfFirstPart;
+            currentDifference = (int) Math.abs(sumOfFirstPart - sumOfSecondPart);
+            minDifference = Math.min(currentDifference, minDifference);
         }
 
-        Collections.sort(newList);
-        return newList.get(0);
+        return minDifference;
     }
 }
