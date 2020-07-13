0292d225e68df3db8fa44c6c60364ee3057430f2
Asad Khan <asad_khan47@hotmail.co.uk>
2020-07-13 18:37:30 +0100

    Still has O(N^2) complexity

--- src/timeComplexity/TapeEquilibrium.java
+++ src/timeComplexity/TapeEquilibrium.java
@@ -12,15 +12,20 @@ public class TapeEquilibrium {
             return 1;
         }
 
-        ArrayList<Integer> newList = new ArrayList<>();
         List<Integer> myArrayList = Arrays.stream(A).boxed().collect(Collectors.toList());
+        ArrayList<Integer> newList = new ArrayList<>();
+        List<Integer> sublist1;
+        List<Integer> sublist2;
+        int sum;
+        int sum2;
+        int absoluteSubtraction;
 
-        for (int i = 0; i < myArrayList.size() - 2; i++) {
-            List<Integer> sublist1 = myArrayList.subList(0, i + 1);
-            List<Integer> sublist2 = myArrayList.subList(i + 1, myArrayList.size());
-            int sum = sublist1.stream().mapToInt(a -> a).sum();
-            int sum2 = sublist2.stream().mapToInt(b -> b).sum();
-            int absoluteSubtraction = Math.abs(sum - sum2);
+        for (int i = 0; i < myArrayList.size() - 1; i++) {
+            sublist1 = myArrayList.subList(0, i + 1);
+            sublist2 = myArrayList.subList(i + 1, myArrayList.size());
+            sum = sublist1.stream().mapToInt(a -> a).sum();
+            sum2 = sublist2.stream().mapToInt(b -> b).sum();
+            absoluteSubtraction = Math.abs(sum - sum2);
             newList.add(absoluteSubtraction);
         }
 
